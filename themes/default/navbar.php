d0ec6a11bddebb71bcfe90aca7f5345dd3a43445
Jarren Long <jarrenlong@gmail.com>
2018-12-06 23:56:28 -0800

    Typos

--- themes/default/navbar.php
+++ themes/default/navbar.php
@@ -3,7 +3,7 @@ function theme_default_navbar($sp, $content) {
   if(!$sp->isAmpPage()) {
 ?>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="<?php echo $this->siteConfig()->authorUrl; ?>">
+      <a class="navbar-brand" href="<?php echo $sp->siteConfig()->authorUrl; ?>">
         <img src="images/sitepipe-512.png" alt="SitePipe" height="44px" />
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
